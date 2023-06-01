3337a7a02e8f5a24e4aca8ab5128f5744d6ca035
tzipiMor <tzipi4040@gmail.com>
2023-06-01 14:17:28 +0300

    finished

--- routes/config_routes.js
+++ routes/config_routes.js
@@ -1,10 +1,12 @@
 const indexR = require("./index");
 const usersR = require("./users");
-// const cakesR = require("./cakes");
+const cakesR = require("./cakes");
+const countriesR = require("./countries");
 
 
 exports.routesInit = (app) => {
     app.use("/", indexR);
     app.use("/users", usersR);
-    // app.use("/cakes", cakesR);
+    app.use("/cakes", cakesR);
+    app.use("/countries", countriesR)
 }
